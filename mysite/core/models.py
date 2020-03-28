f86a41b235d4af1e945df25b6053eef9d3ecdb43
arjun <arjunadappa007@gmail.com>
2020-03-28 22:51:08 +0530

    pageextraction

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    pdf = models.FileField(upload_to='books/pdfs/')
+    pdf = models.FileField(upload_to='')
     cover = models.ImageField(upload_to='books/covers/', null=True, blank=True)
+    page = models.CharField(max_length=100,default='')
 
     def __str__(self):
         return self.title
--- mysite/core/views.py
+++ mysite/core/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.http import FileResponse
 from .forms import BookForm
 from .models import Book
-
+from pdfrw import PdfReader, PdfWriter, IndirectPdfDict
 import os
 import PyPDF2
 import zipfile
@@ -19,17 +19,17 @@ def book_list(request):
         books = Book.objects.all()
         book_num = len(books)
         
-        pdfMerger = PyPDF2.PdfFileMerger() 
+        writer = PdfWriter()
 
         for book in books:
                 if book:
-                    pdfFileObj = PyPDF2.PdfFileReader(book.pdf)
-                    pdfMerger.append(pdfFileObj)   
-        with open(os.path.join('media', 'merged_file.pdf'), 'wb') as pdfOutputFile:
-                pdfMerger.write(pdfOutputFile)
-        response = FileResponse(open(os.path.join('media', 'merged_file.pdf'), 'rb'))
+                     writer.addpages(PdfReader(book.pdf).pages)
+
+        with open(os.path.join('media', 'mergedfile.pdf'), 'wb') as pdfOutputFile:
+                writer.write(pdfOutputFile)
+        response = FileResponse(open(os.path.join('media', 'mergedfile.pdf'), 'rb'))
         response['content_type'] = "application/octet-stream"
-        response['Content-Disposition'] = 'attachment; filename="merged_file.pdf"'
+        response['Content-Disposition'] = 'attachment; filename="mergedfile.pdf"'
 
         return response    
         
@@ -44,7 +44,24 @@ def upload_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            form2 = form.save(commit=False)
+            inpfn = form.cleaned_data['pdf']
+            print(inpfn)
+            
+            page_range = [int(y) for y in form.cleaned_data['page'].split('-')]
+            page_start = int(page_range[0])
+            page_end = int(page_range[1])
+            path = os.path.join('/books/pdfs', 'extracted_page_{}-{}.pdf'.format(page_start, page_end))
+            outfn = os.path.join('media', 'extracted_page_{}-{}.pdf'.format(page_start, page_end))
+            pages = PdfReader(inpfn).pages
+            outdata = PdfWriter(outfn)  
+            page_range = (page_range + page_range[-1:])[:2]
+    
+            for pagenum in range(page_range[0], page_range[1]+1):
+                outdata.addpage(pages[pagenum-1])
+            outdata.write()
+            form2.pdf = os.path.join('extracted_page_{}-{}.pdf'.format(page_start, page_end))
+            form2.save()
             return redirect('book_list')
     else:
         form = BookForm()
