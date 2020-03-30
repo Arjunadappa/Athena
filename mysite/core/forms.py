082b1714f2d0a84def22587689572feb6228e481
arjun <arjunadappa007@gmail.com>
2020-03-31 00:30:47 +0530

    form-update

--- mysite/core/forms.py
+++ mysite/core/forms.py
@@ -7,3 +7,8 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ('title', 'author', 'pdf', 'cover','page')
+
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+        self.fields['page'].widget.attrs['placeholder'] = 'eg:1-2'
+        
